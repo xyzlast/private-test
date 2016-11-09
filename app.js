db44d643f4ca9c3321d744645124513ef5501862
ykyoon <xyzlast@gmail.com>
2016-11-09 17:30:41 +0900

    change comment

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ const ServerBuilder = require('./app/servers/serverBuilder');
 const EventDispatcher = require('./app/events/eventDispatcher');
 
 EventDispatcher.on('process-event', event => {
+  if (event.sequence === 1) {
+    console.log('start - process event');
+  }
   if (event.sequence === 10000000) {
-    console.log('all event(count: 10000000) process completed');
+    console.log('end - all event(count: 10000000) process completed');
+    console.log('====================================================');
     console.log('\\o/ exit application (xyzlast@gmail.com) \\o/');
     process.exit(0);
-  }
-});
-ServerBuilder.buildEventServer(9090, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('start EventServer: port(9090)');
   }
 });
 ServerBuilder.buildUserServer(9099, (err) => {
@@ -23,5 +20,13 @@ ServerBuilder.buildUserServer(9099, (err) => {
     console.error(err);
   } else {
     console.log('start UserServer: port(9099)');
+  }
+});
+ServerBuilder.buildEventServer(9090, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('start EventServer: port(9090)');
+    console.log('waiting events...');
   }
 });
